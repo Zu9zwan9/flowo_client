67f6fcaaff6897cb72895f15c1a217f930c46db9
MrBlack <boichuk.mykhailo@lll.kpi.ua>
2024-12-30 16:25:41 +0200

    Replace isar with hive

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -1,23 +1,33 @@
-import 'package:isar/isar.dart';
+import 'package:hive/hive.dart';
 import 'package:uuid/uuid.dart';
 
-part 'event_model.g.dart';
+part 'event_model.g.dart'; // Генерация кода Hive для модели
 
-@Collection()
+@HiveType(typeId: 0) // Указываем уникальный идентификатор типа данных
 class Event {
-  Id id = Isar.autoIncrement;
-
+  @HiveField(0) // Указываем номер поля
   late String title;
+
+  @HiveField(1)
   String? description;
+
+  @HiveField(2)
   late DateTime startTime;
+
+  @HiveField(3)
   late DateTime endTime;
-  late String category; // Add this line
+
+  @HiveField(4)
+  late String category;
+
+  @HiveField(5)
+  late String id; // Уникальный идентификатор
 
   Event({
     required this.title,
     this.description,
     required this.startTime,
     required this.endTime,
-    required this.category, // Add this line
-  });
+    required this.category,
+  }) : id = Uuid().v4(); // Генерация уникального ID через UUID
 }
